7aaabf4f512f5de46f7453992ab2adc311cab4fe
khsily <khsilyqwerty@gmail.com>
2021-10-06 12:25:39 +0900

    Fix auto complete not responding bug

--- client/src/components/Search/SearchInput/styles.js
+++ client/src/components/Search/SearchInput/styles.js
@@ -1,53 +1,9 @@
-import { Dimensions, Platform, StyleSheet } from "react-native";
+import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
     container: {
-        position: 'relative',
         flexDirection: 'row',
-        width: '100%',
         height: 30,
-    },
-
-    leftContainer: {
-        flex: 1,
-    },
-
-    inputPosWrapper: {
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        zIndex: 1,
-        width: '100%',
-    },
-
-    inputContainer: {
-        height: '100%',
-    },
-
-    input: {
-        height: 30,
-        paddingHorizontal: 8,
-        borderWidth: 0,
-    },
-
-    listContainer: {
-        top: 5,
-        right: 0,
-
-        ...Platform.select({
-            ios: {
-                width: Dimensions.get('window').width - 28,
-            },
-            android: {
-                left: -10,
-                width: Dimensions.get('window').width - 8,
-            },
-        })
-    },
-
-    list: {
-        padding: 5,
-        paddingHorizontal: 10,
     },
 
     button: {
