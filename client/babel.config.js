a5174b4ce79e8fc0cbd90229f8c3b62990cd66f0
khsily <khsilyqwerty@gmail.com>
2021-10-12 13:36:36 +0900

    Add dotenv settings

--- client/babel.config.js
+++ client/babel.config.js
@@ -3,12 +3,13 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      [
-        "@babel/plugin-proposal-decorators",
-        {
-          "legacy": true
-        }
-      ]
+      ["module:react-native-dotenv", {
+        moduleName: "@env",
+        path: ".env",
+      }],
+      ["@babel/plugin-proposal-decorators", {
+        "legacy": true,
+      }]
     ]
   };
 };
