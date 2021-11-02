f9bfbed7e2c4e3a0bc9f021398d0f9afd5413a53
leyel96 <ttt6774@naver.com>
2021-11-02 16:07:26 +0900

    layer modification to avoid overfitting

--- models/detection/yolo_f/tf2_keras_yolo3/yolo_train.py
+++ models/detection/yolo_f/tf2_keras_yolo3/yolo_train.py
@@ -227,18 +227,25 @@ def tiny_yolo_body(inputs, num_anchors, num_classes):
     x1 = compose(
         DarknetConv2D_BN_Leaky(16, (3, 3)),
         MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='same'),
+        Dropout(0.5),
         DarknetConv2D_BN_Leaky(32, (3, 3)),
         MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='same'),
+        Dropout(0.5),
+
         DarknetConv2D_BN_Leaky(64, (3, 3)),
         MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='same'),
+        Dropout(0.5),
         DarknetConv2D_BN_Leaky(128, (3, 3)),
         MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='same'),
+        Dropout(0.5),
         DarknetConv2D_BN_Leaky(256, (3, 3)))(inputs)
     x2 = compose(
         MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='same'),
         DarknetConv2D_BN_Leaky(512, (3, 3)),
+        Dropout(0.5),
         MaxPooling2D(pool_size=(2, 2), strides=(1, 1), padding='same'),
         DarknetConv2D_BN_Leaky(1024, (3, 3)),
+        Dropout(0.5),
         DarknetConv2D_BN_Leaky(256, (1, 1)))(x1)
     y1 = compose(
         DarknetConv2D_BN_Leaky(512, (3, 3)),
